da73ba4f58aa4130b0decfe603379dab19947917
Eliot Clarke <eliot.clarke@fortyau.com>
2021-03-01 14:17:19 -0600

    refactored app to use react router and components correctly. removed excess and commented code. memoized nav component.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink,
 } from "react-router-dom";
 import HomeImg from "./assets/Home.png";
 import DestinationImg from "./assets/Destination1.png";
 import travelToVid from "./assets/travel-to-1.mp4";
 import returnFromVid from "./assets/return-from-1.mp4";
 import styled from "styled-components";
-import DesinationComponent from "./components/DestinationComponent";
+import DestinationComponent from "./components/DestinationComponent";
 import NavBarComponent from "./components/NavBarComponent";
 
 const destination = {
@@ -23,7 +22,7 @@ const destination = {
 };
 
 const initialState = {
-  userLocation: "home",
+  homeImage: HomeImg,
   destinations: [destination],
   modalStatus: false,
 };
@@ -36,154 +35,34 @@ const StyledHomeImage = styled.img`
   height: auto;
 `;
 
-const StyledVideo = styled.video`
-  position: absolute;
-  display: block;
-  z-index: ${(props) => (props.isDisplaying ? "1" : "0")};
-  max-width: 100%;
-  height: auto;
-`;
-
-const StartButton = styled.button`
-  display: ${(props) => (props.buttonDisplay ? "block" : "none")};
-  position: absolute;
-  z-index: ${(props) => (props.zIndexValue ? "0" : "10")};
-  top: 25px;
-  left: 25px;
-`;
-
-const ReturnButton = styled.button`
-  display: block;
-  position: absolute;
-  z-index: 10;
-  top: 25px;
-  right: 25px;
-`;
-
 const App = () => {
-  const [buttonDisplay, setButtonDisplay] = useState(true);
-  const [videoSource, setVideoSource] = useState(travelToVid);
-  const [imageSource, setImageSource] = useState(HomeImg);
-  const [isDisplaying, updateIsDisplaying] = useState(false);
-  const [isVideoPlaying, updateIsVideoPlaying] = useState(false);
-  const vidRef = useRef(null);
-
-  function handleNavigateToClick(e) {
-    e.preventDefault();
-    updateIsDisplaying(!isDisplaying);
-    toggleImageSrc();
-    vidRef.current.play();
-    updateIsVideoPlaying(true);
-  }
-
-  const toggleImageSrc = () => {
-    if (imageSource === HomeImg) {
-      setImageSource(DestinationImg);
-    } else {
-      setImageSource(HomeImg);
-    }
-  };
-
-  const toggleVideoSrc = () => {
-    if (videoSource === travelToVid) {
-      setVideoSource(returnFromVid);
-    } else {
-      setVideoSource(travelToVid);
-    }
-  };
 
   return (
-    <>
       <Router>
-        <NavBarComponent />
+        <NavBarComponent destinations={initialState.destinations} />
         <div>
           <Switch>
             <Route exact path="/">
               <Home />
             </Route>
-            <Route path="/destination1">
-              <Destination1 />
-            </Route>
+            {initialState.destinations &&
+              initialState.destinations.map((destination) => {
+                return (
+                  <Route key={destination.name} path={`/${destination.name}`}>
+                    <DestinationComponent destination={destination} />
+                  </Route>
+                );
+              })}
           </Switch>
         </div>
-
-        {/* <StartButton
-          onClick={handleNavigateToClick}
-          buttonDisplay={buttonDisplay}
-          zIndexValue={isVideoPlaying}
-        >
-          START
-        </StartButton>
-        <StyledHomeImage
-          src={imageSource}
-          alt="booth scene image"
-          isDisplaying={isDisplaying}
-        />
-        {initialState.destinations &&
-          initialState.destinations.map((destination) => (
-            <DesinationComponent
-              destinationObj={destination}
-              handleNavigateToClick={handleNavigateToClick}
-            />
-          ))} */}
       </Router>
-    </>
   );
 };
 
 const Home = () => {
   return (
     <div>
-      <StyledHomeImage src={HomeImg}/>
-    </div>
-  );
-};
-
-const Destination1 = () => {
-  const [buttonDisplay, setButtonDisplay] = useState(true);
-  const [videoSource, setVideoSource] = useState(travelToVid);
-  const [imageSource, setImageSource] = useState(HomeImg);
-  const [isDisplaying, updateIsDisplaying] = useState(false);
-  const [isVideoPlaying, updateIsVideoPlaying] = useState(false);
-  const vidRef = useRef(null);
-
-  function handleNavigateToClick() {
-    updateIsDisplaying(!isDisplaying);
-    vidRef.current.play();
-    updateIsVideoPlaying(true);
-  }
-
-  const toggleVideoSrc = () => {
-    if (videoSource === travelToVid) {
-      setVideoSource(returnFromVid);
-    } else {
-      setVideoSource(travelToVid);
-    }
-  };
-
-  useEffect(() => {
-    console.log("use effect");
-    handleNavigateToClick();
-  }, []);
-
-  // ! this could work
-  // ! just create a return button/close modal function that retriggers playing the video - then use a redirect to home?
-
-  return (
-    <div>
-      <StyledVideo
-        ref={vidRef}
-        src={videoSource}
-        isDisplaying={isDisplaying}
-        onEnded={() => {
-          updateIsVideoPlaying(false);
-          setButtonDisplay(!buttonDisplay);
-          updateIsDisplaying(!isDisplaying);
-          toggleVideoSrc();
-        }}
-      />
-      <StyledHomeImage src={DestinationImg} isDisplaying={!isDisplaying} />
-      <ReturnButton onClick={handleNavigateToClick}>RETURN</ReturnButton>
+      <StyledHomeImage src={HomeImg} />
     </div>
   );
 };
