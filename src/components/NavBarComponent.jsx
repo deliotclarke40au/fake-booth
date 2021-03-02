239af4dbfb61ed1cde6dc41355dbc6544acdd646
Eliot Clarke <eliot.clarke@fortyau.com>
2021-03-02 13:52:38 -0600

    started refactoring to remove react router - had an idea to create a video component - moved all code over to elevation repo.

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -9,37 +9,47 @@ const NavBarContainer = styled.div`
   z-index: 10;
 `;
 
+const NavButton = styled.button`
+  background: tomato;
+  color: white;
+  border-radius: 10px;
+  padding: 1rem 0.5rem;
+`;
+
 const NavBarComponent = ({ destinations }) => {
   const { pathname } = useLocation();
   console.log(pathname, destinations);
-  return useMemo(() => (
-    <NavBarContainer>
-      <nav>
-        <ul>
-          <li>
-            {/* TODO create a return home function when this item is clicked that plays the video based on current location before directing? if user is already home disable item? */}
-            <NavLink className="nav-item" activeClassName="is-active" to="/">
-              Home
-            </NavLink>
-          </li>
-          {destinations &&
-            destinations.map((destination) => {
-              return (
-                <li key={destination.name}>
-                  <NavLink
-                    className="nav-item"
-                    activeClassName="is-active"
-                    to={destination.name}
-                  >
-                    {destination.name}
-                  </NavLink>
-                </li>
-              );
-            })}
-        </ul>
-      </nav>
-    </NavBarContainer>
-  ), [destinations]);
+  return useMemo(
+    () => (
+      <NavBarContainer>
+        <nav>
+          <ul>
+            <li>
+              {/* TODO create a return home function when this item is clicked that plays the video based on current location before directing? if user is already home disable item? */}
+              <NavLink className="nav-item" activeClassName="is-active" to="/">
+                Home
+              </NavLink>
+            </li>
+            {destinations &&
+              destinations.map((destination) => {
+                return (
+                  <li key={destination.name}>
+                    <NavLink
+                      className="nav-item"
+                      activeClassName="is-active"
+                      to={destination.name}
+                    >
+                      {destination.name}
+                    </NavLink>
+                  </li>
+                );
+              })}
+          </ul>
+        </nav>
+      </NavBarContainer>
+    ),
+    [destinations]
+  );
 };
 
 export default NavBarComponent;
